78bd2a7d2d84919a2650629f977ed1631e6f7b9d
Artem <artem.a.gavrilov@gmail.com>
2026-05-02 16:30:00 +0300

    Add simple Python web server

---
+++ cgi-bin/time.py
@@ -0,0 +1,11 @@
+import datetime
+
+print("Content-Type: text/html")
+print()
+
+print("<html>")
+print("<body>")
+print("<h1>Current Time</h1>")
+print(datetime.datetime.now())
+print("</body>")
+print("</html>")
